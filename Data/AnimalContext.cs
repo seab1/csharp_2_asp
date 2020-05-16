ca909c7719e459a74ab0344b6796d15f15b695d8
Sebastian Suwala <sebastian.suwala1@gmail.com>
2020-05-16 15:00:27 +0200

    III.g done

---
+++ Data/AnimalContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SSuwalaBonjourLeMonde.Models;
+
+namespace SSuwalaBonjourLeMonde.Data
+{
+    public class AnimalContext : DbContext
+    {
+        public AnimalContext (DbContextOptions<AnimalContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SSuwalaBonjourLeMonde.Models.Animal> Animal { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using SSuwalaBonjourLeMonde.Data;
 
 namespace SSuwalaBonjourLeMonde
 {
@@ -23,6 +25,9 @@ namespace SSuwalaBonjourLeMonde
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<AnimalContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("AnimalContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
