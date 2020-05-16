c8094f7bf33cd0472e57df3c4725cd258079e8d2
Sebastian Suwala <sebastian.suwala1@gmail.com>
2020-05-16 13:12:42 +0200

    All until I.k and I.l done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace SSuwalaBonjourLeMonde.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public String Bonjour()
+        {
+            return "Let's go!";
+        }
     }
 }
