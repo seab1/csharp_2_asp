9bf2a40a7bfab4b79493510cef4537974d95f82f
Sebastian Suwala <sebastian.suwala1@gmail.com>
2020-05-16 12:59:44 +0200

    Project created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SSuwalaBonjourLeMonde.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
