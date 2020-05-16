9bc5e7108cdcb3f8cf7a1e530296e9089b8de166
Sebastian Suwala <sebastian.suwala1@gmail.com>
2020-05-16 15:32:27 +0200

    IV.g done

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,8 @@ namespace SSuwalaBonjourLeMonde.Models
     {
         public int ID { get; set; }
         public String Name { get; set; }
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
     }
 }
